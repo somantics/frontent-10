e055605d0d54a70518b560730da4fed736128401
Amos Johan Persson <amosjohanpersson@gmail.com>
2026-08-14 15:12:51 +0200

    add tracking for total count

--- src/components/GoalTracker.tsx
+++ src/components/GoalTracker.tsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
 import ProgressBar from "./ProgressBar";
+import { CounterContext } from "../CounterContext";
 
 function GoalTracker() {
+  const counters = useContext(CounterContext);
+  const sum = counters.reduce((total, item) => total + item.count, 0);
   return (
     <section>
       <p>Progress</p>
-      <p>7/10</p>
+      <p>{sum}/10</p>
       <ProgressBar />
     </section>
   );
