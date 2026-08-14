1a3a0263ba9328e06a5c3ae852f90956bb03a057
Amos Johan Persson <amosjohanpersson@gmail.com>
2026-08-14 13:31:39 +0200

    reorder component tree for better context sharing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import "./App.css";
-import Accumulator from "./components/Accumulator";
 import CounterSection from "./components/CounterSection";
-import GoalTracker from "./components/GoalTracker";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
-      <GoalTracker />
-      <Accumulator />
       <CounterSection />
     </>
   );
--- src/components/CounterSection.tsx
+++ src/components/CounterSection.tsx
@@ -1,16 +1,36 @@
+import { createContext, useState } from "react";
+import Accumulator from "./Accumulator";
 import CounterGrid from "./CounterGrid";
+import GoalTracker from "./GoalTracker";
 import PrimaryButton from "./PrimaryButton";
 import SecondaryButton from "./SecondaryButton";
 
+interface CounterData {
+  id: number;
+  count: number;
+  max: number;
+}
+
+const defaultCounters = [
+  { id: 0, count: 0, max: 3 } as CounterData,
+  { id: 1, count: 0, max: 3 } as CounterData,
+  { id: 2, count: 0, max: 3 } as CounterData,
+  { id: 3, count: 0, max: 3 } as CounterData,
+];
+
+const CounterContext = createContext(defaultCounters);
+
 function CounterSection() {
   return (
-    <section>
+    <CounterContext value={defaultCounters}>
+      <GoalTracker />
+      <Accumulator />
       <div>
         <PrimaryButton text="+ Lägg till räknare" onClick={() => {}} />
         <SecondaryButton text="Nollställ alla räknare" onClick={() => {}} />
       </div>
       <CounterGrid />
-    </section>
+    </CounterContext>
   );
 }
 
